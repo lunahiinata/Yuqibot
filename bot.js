e875a50b3bb54ae9f27c1b0291f2dc539c6a664a
lunahiinata <60007337+lunahiinata@users.noreply.github.com>
2020-01-17 20:50:49 +0400

    UPDATE

--- bot.js
+++ bot.js
@@ -1,19 +1,29 @@
-/*
- * Discord Bot Builder Bot
- * Version 1.2.0
- * Robert Borghese
- */
+const Discord = require('discord.js');
+// Importing this allows you to access the environment variables of the running node process
+require('dotenv').config();
 
-const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
+const client = new Discord.Client();
 
-if(!process.send) {
+// "process.env" accesses the environment variables for the running node process. PREFIX is the environment variable you defined in your .env file
+const prefix = process.env.PREFIX;
 
-Files.initStandalone();
-
-} else {
-
-process.on('message', function(content) {
-	Files.initBotTest(content);
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-}
+client.on('message', message => {
+
+  // Here's I'm using one of An Idiot's Guide's basic command handlers. Using the PREFIX environment variable above, I can do the same as the bot token below
+  if (message.author.bot) return;
+  if (message.content.indexOf(prefix.length) !== 0) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if (command === 'ping') {
+    message.reply('Pong!');
+  }
+});
+
+// Here you can login the bot. It automatically attempts to login the bot with the environment variable you set for your bot token (either "CLIENT_TOKEN" or "DISCORD_TOKEN")
+client.login(BOT_TOKEN);
